594aefc245d66f62fe6a340c0196496fa4fdc1d8
MabEntez <106976240+MabEntez@users.noreply.github.com>
2023-10-23 17:35:45 +0100

    Add files via upload

--- Cyst_per_sheep.R
+++ Cyst_per_sheep.R
@@ -1,25 +1,35 @@
+# Clear the current R environment
 rm(list = ls())
+
+# Load the necessary libraries
 library(rstudioapi)
 library(dplyr)
 library(ggplot2)
 library(cowplot)
 library(gridExtra)
 
-#### Data Prep ####
+#### Data Preparation ####
+
+# Set working directory to the directory of the currently open script
 setwd(dirname(getActiveDocumentContext()$path))
 
+# Read the CSV data
 model_output <- read.csv("Cyst and Proto.csv")
 
+# Filter the data for a specific step
 model_output <- filter(model_output, Step == ((20 * 24)-10))
+
+# Filter out the sheep data and select relevant columns
 sheep_output <- filter(model_output, Species == "sheep")
 sheep_output <- sheep_output[, c(3,6)]
+
+# Filter out the dog data, select relevant columns and remove rows where N.parasite is 0
 dog_output <- filter(model_output, Species == "dog")
 dog_output <- dog_output[, c(4,6)]
 dog_output <- dog_output %>% 
   filter(N.parasite != 0)
 
-
-# convert strings to list
+# Convert the cyst age list from string to list
 sheep_output$Cyst.age.list <- strsplit(sheep_output$Cyst.age.list, " ")
 sheep_output$Cyst.age.list <- lapply(sheep_output$Cyst.age.list, function(x) {
   x <- gsub("\\[", "", x) # Removes the opening bracket
@@ -27,23 +37,27 @@ sheep_output$Cyst.age.list <- lapply(sheep_output$Cyst.age.list, function(x) {
   as.numeric(unlist(x)) 
 })
 
+# Set constants for cyst and proto calculations
 nCystPerYear <-  2.21
 ProtPerCyst <-  10.68
 
+# Increment the second column of sheep_output
 sheep_output[,2] <- sheep_output[,2] + 1
 
+# Calculate the Protoscolex count and Total Cysts
 for (i in 1:nrow(sheep_output)){
   sheep_output[i,3] <- nCystPerYear*sum(as.numeric(unlist(sheep_output[i,1]))*as.numeric(sheep_output[i,2])^3)
   sheep_output[i,4] <- sum(as.numeric(unlist(sheep_output[i,1])))
   print(i)
 }
-  
+
+# Rename columns and filter rows with zeros
 sheep_output <- sheep_output %>% rename(Protoscolex_count = V3,
                                         Total_Cysts = V4)
-#model_output[,4] <- as.numeric(model_output[,4])
 sheep_output <- subset(sheep_output, Total_Cysts != 0)
 sheep_output <- subset(sheep_output, Protoscolex_count != 0)
 
+# Create a summary dataframe with aggregated data
 summary_df <- sheep_output %>%
   group_by(Age) %>%
   summarise(Mean_Cysts = mean(Total_Cysts, na.rm = TRUE),
@@ -53,6 +67,7 @@ summary_df <- sheep_output %>%
             uq_Proto = quantile(Protoscolex_count, 0.95, na.rm = TRUE),
             lq_Proto = quantile(Protoscolex_count, 0.05, na.rm = TRUE))
 
+# Plot Total Protoscolex in Infected Sheep by Age
 cyst <- ggplot(summary_df, aes(x = Age, y = Mean_Proto)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   geom_errorbar(aes(ymin = lq_Proto, ymax = uq_Proto), 
@@ -64,12 +79,11 @@ cyst <- ggplot(summary_df, aes(x = Age, y = Mean_Proto)) +
        title = "Total Protoscolex in Infected Sheep by Age") +
   theme_bw()
 
-# Compute the mean values in a new data frame
+# Create a violin plot for the number of mature cysts within infected sheep
 mean_values <- sheep_output %>%
   group_by(Age) %>%
   summarise(mean_Total_Cysts = mean(Total_Cysts, na.rm = TRUE))
 
-# Create the plot
 proto <- ggplot(sheep_output, aes(x = factor(Age), y = Total_Cysts)) +
   geom_violin(fill = "darkblue", alpha = 0.5) +
   geom_point(data = mean_values, aes(y = mean_Total_Cysts, color = "black", shape = "Mean"), size = 3) +
@@ -79,6 +93,7 @@ proto <- ggplot(sheep_output, aes(x = factor(Age), y = Total_Cysts)) +
   scale_y_continuous(breaks = c(0, 2, 4, 6, 8, 10, 12), limits = c(1, 12)) +
   theme_bw()
 
+# Combine the two plots
 combined_plot <- plot_grid(
   cyst, 
   proto, 
@@ -86,8 +101,7 @@ combined_plot <- plot_grid(
   align = "h"
 )
 
-
-
+# Plot the density of adults parasite count in infected dogs
 ggplot(dog_output, aes(x=N.parasite)) + 
   geom_density(fill="blue", alpha=0.5) +
   labs(title="Adults parasite count in infected dogs", x="Number of parasites", y="Density")
